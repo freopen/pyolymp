8179b1dd5224852e04eae1d4efc8a55cfaefe3c3
Alexey Zolotov <freopen@freopen.org>
2012-07-17 23:20:08 +0400

    Deploy script created. It uses simple internal CherryPy http server.

--- http_server/__init__.py
+++ http_server/__init__.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
+"This module is simple HTTP server"
 import cherrypy
+import sys
 
+
+def start_http_server(handler, config):
+    """Starts HTTP server.
+
+    handler
+        A class, which describes functions to serve HTTP queries in
+        cherrypy style.
+    config
+        A path to config file in cherrypy style.
+    """
+    cherrypy.tree.mount(handler)
+    cherrypy.config.update(config)
+    engine = cherrypy.engine
+    try:
+        engine.start()
+    except:
+        sys.exit(1)
+    else:
+        engine.block()
