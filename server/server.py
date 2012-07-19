384f6d4878aedb4372a3cc8b84a7f854e5709008
Alexey Zolotov <freopen@freopen.org>
2012-07-19 22:39:09 +0400

    Mako template system added.

--- server/server.py
+++ server/server.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python3
 from pyolymp import http_server
+from mako.lookup import TemplateLookup
+from configparser import ConfigParser
 
-class Test:
+
+class Site:
+    def __init__(self, template_dir=None):
+        self.template_lookup = \
+                TemplateLookup(directories=[template_dir])
+        self.config = ConfigParser().read("conf/site.conf")
+
     def index(self):
-        return "Hello, world"
+        return self.template_lookup.get_template("page.html").render(\
+                title="Hello", \
+                page="Hello, world!")
     index.exposed = True
 
-http_server.start_http_server(Test(), "conf/http_server.conf")
+def main():
+    config = ConfigParser()
+    config.read("conf/server.conf")
+    site = Site(template_dir = config["paths"]["template_dir"])
+    http_server.start_http_server(site, "conf/http_server.conf")
+
+if __name__ == "__main__":
+    main()
